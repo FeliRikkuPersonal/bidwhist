85250b8a3864134d6907b3bfa30c44e59c37ba18
Felicia <fg@ngi1.com>
2025-07-18 09:52:10 -0400

    Setting up dev and prod switches in main

--- bidwhist-backend/src/main/java/com/bidwhist/dto/WebConfig.java
+++ bidwhist-backend/src/main/java/com/bidwhist/dto/WebConfig.java
@@ -1,25 +1,28 @@
 package com.bidwhist.dto;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
+
+    @Value("${cors.allowed.origin}")
+    private String allowedOrigin;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("https://bidwhist.netlify.app")
+                        .allowedOrigins(allowedOrigin)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(false); // change to true if sending cookies/auth headers
+                        .allowCredentials(false); // set to true if you're sending cookies or auth headers
             }
         };
     }
 }
-
